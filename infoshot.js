b69f20d2f58269a970cd8b4911768f8b1c9c1236
Paul A <jimmyyukka@gmail.com>
2019-04-01 11:59:19 +0200

    First published Infoshote
    This now has remved the Avatar section . json links

--- infoshot.js
+++ infoshot.js
@@ -1,69 +1,83 @@
-//THIS IS ALL 1 MASTER FUNCTION WITH  - SUB FUNCTIONS //
+
+//get data from JSON //
+
 $.getJSON(
-    "data/chang.json",
-    function(data) {
-        factData = data;
-        //Order Data Components //
-        var dataToRenderForFacts = [];
-        var filteredFacts = factData.filter(x => x.columnName == "Facts");
-        var filteredFors = factData.filter(x => x.columnName == "For");
-        var filteredAgainst = factData.filter(x => x.columnName == "Against");
-//Func1 //
-        function orderData(data, resultArray) {
-            for (var index = 0; index < data.length; index++) {
-                data[index].children = [];
-                data[index].children.push(data[index]);
+  "data/changSubject.json",
+  function(data) {
 
-                for (var index2 = index + 1; index2 < data.length; index2++) {
-                    if (data[index].subjectId == data[index2].subjectId) {
-                        data[index2].isprocessed = "true";
-                        data[index].children.push(data[index2]);
-                    }
-                }
-                if (!data[index].isprocessed)
-                    resultArray.push(data[index]);
-            }
-            console.log(resultArray);
-        }
-//Func2 //
-        function displayFacts(facts) {
-        }
+factData = data;
+//query data from factdata //
+var dataToRenderForFacts = [];
+var filteredFacts = factData.filter(x => x.columnName == "Facts");
+var filteredFors = factData.filter(x => x.columnName == "For");
+var filteredAgainst = factData.filter(x => x.columnName == "Against");
+var question = factData.filter(x => x.columnName == "Question");
+//OUTPUT INFOSHOT TITE TO HTML //
+$('#question').html(question[0].linkName);
+//CREATE THE REST OF THE HTML //
+function orderData(data, resultArray){
+  for(var index = 0; index < data.length; index ++){
+    data[index].children = [];
+    data[index].children.push(data[index]);
 
-//Func3 //
-        function columnName(pet) {
-            return `<div class="blue article-header">${pet.columnName}</div>`
+    for(var index2 = index + 1; index2 < data.length; index2++)
+    {
+      if(data[index].subjectId == data[index2].subjectId)
+      {
+        data[index2].isprocessed = "true";
+        data[index].children.push(data[index2]);
+      }
+    }
+    if(!data[index].isprocessed)
+    resultArray.push(data[index]);
+  }
+  console.log(resultArray);
+}
 
-        }
+function displayFacts(facts) {
+  
+}
 
+//Declare variables //
+function columnName(pet){
+    
+  return `<div class="blue article-header">${pet.columnName}</div>`
 
+}
 
+function factTemplate(pet){
+    
+  var html = `<div class="subject-header">${pet.subjectName}</div>`;
+  pet.children.forEach(element => {
+    html += `<a href="${element.linkUrl}" target="_blank" class ="item"><div class="item-container">${element.linkName} </div></a>`
 
-//Output Func Column 1 //
-        function factTemplate(pet) {
-            var html = `<div class="subject-header">${pet.subjectName}</div>`;
-            pet.children.forEach(element => {
-                html += `<a href="${element.linkUrl}" target="_blank" class ="item"><div class="item-container">${element.linkName} </div></a>`
+  });
+  return html;
 
-            });
-            return html;
-        }
-        orderData(filteredFacts, dataToRenderForFacts);
-        document.getElementById("facts").innerHTML = `
+  
+}
+orderData(filteredFacts, dataToRenderForFacts);
+document.getElementById("facts").innerHTML = `
 <div class="blue article-header">${dataToRenderForFacts[0].columnName}</div>
+
 ${dataToRenderForFacts.map(factTemplate).join("")}
+
+
+
 `;
-//Output Func Column 2 //
-        var dataToRenderForFors = [];
-        orderData(filteredFors, dataToRenderForFors);
-        document.getElementById("facts2").innerHTML = `
+var dataToRenderForFors = [];
+orderData(filteredFors, dataToRenderForFors);
+document.getElementById("facts2").innerHTML = `
 <div class="green article-header">${dataToRenderForFors[0].columnName}</div>
 
 ${dataToRenderForFors.map(factTemplate).join("")}
+
+
+
 `;
-//Output Func Column 2 //
-        var dataToRenderForAgainst = [];
-        orderData(filteredAgainst, dataToRenderForAgainst);
-        document.getElementById("facts3").innerHTML = `
+var dataToRenderForAgainst = [];
+orderData(filteredAgainst, dataToRenderForAgainst);
+document.getElementById("facts3").innerHTML = `
 <div class="red article-header">${dataToRenderForAgainst[0].columnName}</div>
 
 ${dataToRenderForAgainst.map(factTemplate).join("")}
@@ -72,5 +86,5 @@ ${dataToRenderForAgainst.map(factTemplate).join("")}
 
 `;
 
-    }
+}
 );
