4c98e6ea16cbfeec351a96af050cfd9e21be6b7f
Paul AYling <jimmyyukka@gmail.com>
2019-04-21 14:34:09 +0200

    includes rework of style.css

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -6,7 +6,7 @@ function setDisplay(className, displayValue) {
     }
 }
 function showActionBar() {
-  var elements = document.getElementsByClassName("toolbar");
+  var elements = document.getElementsByClassName("all-tools");
   Array.prototype.forEach.call(elements, function(el) {
     if (el.style.display === "none") {
       el.style.display = "flex";
@@ -15,6 +15,9 @@ function showActionBar() {
     }
   });
 }
+
+
+
 
 function setDisplay(className, displayValue) {
     var items = document.getElementsByClassName(className);
@@ -34,6 +37,31 @@ function showIcons() {
 }
 
 
+
+
+
+function setDisplay(className, displayValue) {
+    var items = document.getElementsByClassName(className);
+    for (var i=0; i < items.length; i++) {
+    items[i].style.display = displayValue;
+    }
+    }
+    function showUserBar() {
+    var elements = document.getElementsByClassName("userBar");
+    Array.prototype.forEach.call(elements, function(el) {
+    if (el.style.display === "none") {
+      el.style.display = "flex";
+    } else {
+      el.style.display = "none";
+    }
+    });
+    }
+
+
+
+
+    
+
 function setDisplay(className, displayValue) {
 var items = document.getElementsByClassName(className);
 for (var i=0; i < items.length; i++) {
@@ -50,6 +78,29 @@ if (el.style.display === "none") {
 }
 });
 }
+
+
+
+
+
+
+
+function showResponsive () {
+    var elements = document.getElementsByClassName("main");
+    Array.prototype.forEach.call(elements, function(el) {
+      if (el.style.flexDirection === "column") {
+          el.style.flexDirection = "row";
+      } else {
+          el.style.flexDirection = "column";
+      }
+    });
+}
+
+
+
+
+
+
 
 function setDisplay(className, displayValue) {
 var items = document.getElementsByClassName(className);
@@ -70,87 +121,3 @@ if (el.style.display === "block") {
 
 
 
-function setDisplay(className, displayValue) {
-var items = document.getElementsByClassName(className);
-for (var i=0; i < items.length; i++) {
-items[i].style.display = displayValue;
-}
-}
-function showUserBar() {
-var elements = document.getElementsByClassName("UserBar");
-Array.prototype.forEach.call(elements, function(el) {
-if (el.style.display === "none") {
-  el.style.display = "flex";
-} else {
-  el.style.display = "none";
-}
-});
-}
-
-
-
-function showResponsive () {
-    var elements = document.getElementsByClassName("main");
-    Array.prototype.forEach.call(elements, function(el) {
-      if (el.style.flexDirection === "column") {
-          el.style.flexDirection = "row";
-      } else {
-          el.style.flexDirection = "column";
-      }
-    });
-}
-
-
-
-
-
-function setDisplay(className, displayValue) {
-    var items = document.getElementsByClassName(className);
-    for (var i=0; i < items.length; i++) {
-    items[i].style.display = displayValue;
-    }
-}
-function showsiteContainer() {
-    var elements = document.getElementsByClassName("siteContainer");
-    Array.prototype.forEach.call(elements, function(el) {
-    if (el.style.display === "none") {
-        el.style.display = "flex";
-    } else {
-        el.style.display = "none";
-    }
-    });
-}
-
-function setDisplay(className, displayValue) {
-    var items = document.getElementsByClassName(className);
-    for (var i=0; i < items.length; i++) {
-    items[i].style.display = displayValue;
-    }
-}
-function showsShareBox() {
-    var elements = document.getElementsByClassName("embed");
-    Array.prototype.forEach.call(elements, function(el) {
-    if (el.style.display === "block") {
-        el.style.display = "none";
-    } else {
-        el.style.display = "block";
-    }
-    });
-}
-
-function setDisplay(className, displayValue) {
-    var items = document.getElementsByClassName(className);
-    for (var i=0; i < items.length; i++) {
-    items[i].style.display = displayValue;
-    }
-}
-function showUserBar() {
-    var elements = document.getElementsByClassName("userBar");
-    Array.prototype.forEach.call(elements, function(el) {
-    if (el.style.display === "none") {
-        el.style.display = "flex";
-    } else {
-        el.style.display = "none";
-    }
-    });
-}
